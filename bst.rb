d8d65e0ca8a9e7baa2877ff5aaa7f53f4ead5456
DNML31 <danielchun31@gmail.com>
2023-04-11 13:29:26 -0700

    Add @root Tree class initialize
    This commit adds the @root instance variable to the Tree class
    initialize method. This variable is equal to the returned node of
    the build_tree class method.
    
    The insert method is still incomplete, however, the pretty_print method
    was added to help visualize whether the build_tree actually works. In
    order for this method to work I had to change '@left_sub' and
    '@right_sub' instance variables in the Node class(and anywhere else it
    references) to be 'left' and 'right'.

--- bst.rb
+++ bst.rb
@@ -11,12 +11,12 @@ end
 class Node
   include Comparable
 
-  attr_accessor :value, :left_sub, :right_sub
+  attr_accessor :value, :left, :right
 
   def initialize(value, left = nil, right = nil)
     @value = value
-    @left_sub = left
-    @right_sub = right
+    @left = left
+    @right = right
   end
 
 end
@@ -24,13 +24,12 @@ end
 class Tree
   attr_accessor :root
   def initialize(array)
-    @array = array
+    array = array.sort!.uniq
     @root = build_tree(array, 0, array.length - 1)
   end
 
-  def build_tree(array, first, last)
-    array.sort!.uniq # sort and remove duplicates
-  
+  def build_tree(array, first = 0, last = array.length - 1)
+
     if first > last
       return nil
     end
@@ -38,26 +37,37 @@ class Tree
     mid = (first + last) / 2
     mid_node = Node.new(array[mid])
 
-    build_tree(array, first, mid - 1)
-    build_tree(array, mid + 1, last)
+    mid_node.left = build_tree(array, first, mid - 1)
+    mid_node.right = build_tree(array, mid + 1, last)
 
-    return mid_node.value
+    return mid_node
+    # didn't work with mid_node.value because that returns '8', not the Node obj
   end
 
-  def insert(value)
-
-    if value == @root
-      puts "can't have duplicate value"
+  def insert(value, root)
+    if root == nil
+      root = Node.new(value)
     end
 
-    if value < @root
+    if value == root
+      puts "can't have duplicate values"
+    end
+
+    if value < root
+      insert(value, root.left)
       # find root of left subtree...
       # go down left subtree
-    else
+    elsif value > root
+      insert(value, root.right)
       # find root of right subtree
       # go down right subtree
     end
+  end
 
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 
 end
@@ -66,9 +76,12 @@ end
 array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 # 1, 3, 4, 5, 7, 8, 9, 23, 67, 324, 6345
 tree = Tree.new(array)
-# create a Tree object with the given array
+puts tree.root
 
-tree.build_tree(array, 0, array.length - 1)
+tree.build_tree(array)
+tree.pretty_print
 
-puts tree.insert(7)
+# tree.build_tree(array)
+
+# puts tree.insert(6, array)
 
