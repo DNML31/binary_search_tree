a5a2a12d63a4d2dab5c39337f738c6aca2904a39
DNML31 <danielchun31@gmail.com>
2023-04-14 10:00:38 -0700

    Tree class insert method complete
    This commit completes the insert method to add a node to the tree.
    
    Initially I kept getting either a SystemStackError, a tree without the
    added node or a tree with just the node itself when using the case
    statement block. I went back to using an if..else block after seeing
    others' work. I think the main reason why the method works now is
    because right after determining if the root.value is less than or
    greater than the value to add, it checks to see if root.right or
    root.left is nil and adds a new Node before calling the recursive
    statement.

--- bst.rb
+++ bst.rb
@@ -35,7 +35,6 @@ class Tree
   end
 
   def build_tree(array, first = 0, last = array.length - 1)
-
     if first > last
       return nil
     end
@@ -51,61 +50,53 @@ class Tree
 
   def insert(value, root = @root)
 
-    # case root
-    # when value == root then return nil
-    # when nil
+    if root.nil?
+      return nil
+    elsif root.value > value
+      root.left = Node.new(value) if root.left.nil?
+      insert(value, root.left)
+    elsif root.value < value
+      root.right = Node.new(value) if root.right.nil?
+      insert(value, root.right)
+    end
+
+    # case @root
+    # when @root.value == value then puts "duplicate"
+    # when root.nil?
     #   @root = Node.new(value)
-    #   return
-    # when root.value > value
-    #   root = root.left
-    #   insert(value)
-    # when root.value < value
-    #   root = root.right
-    #   insert(value)
+    # when @root.value > value
+    #   insert(value, root.left)
+    # when @root.value < value
+    #   insert(value, root.right)
     # else
     #   return
     # end
 
-    if @root == nil
-      @root = Node.new(value)
-    end
+    # if root.nil? 
+    #   @root = Node.new(value)
+    # end
 
-    if value == @root.value
-      return nil
-    end
-
-    if value < @root.value
-      if @root.value.nil?
-        @root.value = Node.new(value)
-      else
-        @root = @root.left
-      end
-
-    elsif value > @root.value
-
-      if @root.value.nil?
-        @root.value = Node.new(value)
-      else
-        @root = @root.right
-      end
-      
-    end
-
-    insert(value, root = @root)
+    # case value
+    # when value == root then print "duplicate"
+    # when value < root
+    #   insert(value, root.left)
+    # when value > root
+    #   insert(value, root.right)
+    # end
 
   end
 
 end
 
 
-# array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 # 1, 3, 4, 5, 7, 8, 9, 23, 67, 324, 6345
 # if it is sorted and no duplicates
 
-array = [1,2,4]
+# array = [1]
 
 tree = Tree.new(array)
 tree.build_tree(array)
-tree.insert(3)
+tree.insert(2)
 
 tree.pretty_print
